2221a538e78464a47b8ad913368a1db8db11a22f
Rohit Verma <101377978+rohit9625@users.noreply.github.com>
2024-04-18 00:30:46 +0530

    update code for netty engine

--- src/main/kotlin/com/devx/Application.kt
+++ src/main/kotlin/com/devx/Application.kt
@@ -3,12 +3,9 @@ package com.devx
 import com.devx.database.Database
 import com.devx.plugins.*
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty, port = System.getenv("PORT").toInt(), host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
